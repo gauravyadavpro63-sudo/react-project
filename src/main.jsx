3844b0df68e0187032a28e9e242c3fcae1e2d20e
Gaurav Yadav <gauravyadavpro63@gmail.com>
2026-06-29 12:36:01 +0530

    bf 2

--- src/main.jsx
+++ src/main.jsx
@@ -6,42 +6,30 @@ import ReactDOM from "react-dom/client"
 // footer
 
 
-function Card(){
+function Card(props){
     return(
 <div style={{border:"2px solid black",padding:"2px"}}>
     <img src="https://imgs.search.brave.com/AfSuwap_n2n0j9W5uxav7y-w3phFX2NnZKaC9EuUZOc/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NTFNaFJSeDJYSUwu/X0FDLl9TUjE4MCwy/MzAuanBn" alt="" height="200px" width="200px" />
     <div style={{textAlign:"center"}}>
-        <h2>T-Shirts</h2>
-        <h2>40-80%off</h2>
+        <h2>{props.cloth}</h2>
+        <h2>{props.offer}</h2>
         <h2>Shop Now</h2>
     </div>
 </div>
     )
 }
 
-
+const arr=[{cloth:"tshirt",offer:"20-30%off"},{cloth:"shirt",offer:"20-40%off"},{cloth:"kurta",offer:"20-90%off"},{cloth:"jeans",offer:"4-6%off"},{cloth:"T-Shirt",offer:"20% OFF"},{cloth:"Shirt",offer:"35% OFF"},{cloth:"Jeans",offer:"50% OFF"},{cloth:"Hoodie",offer:"15% OFF"},{cloth:"Kurta",offer:"40% OFF"},{cloth:"Jacket",offer:"60% OFF"}];
 
 function App(){
 return (
     // header
 
  <div style={{display:"flex",gap:"10px",flexWrap:"wrap"}}>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-   
+  {
+    arr.map((value,index)=> <Card cloth={value.cloth} offer={value.offer}/>)
+  
+  }
     </div>
     // footer
 )
