951362e53175aa5762d1f4a8b38ccd7781282f69
Gaurav Yadav <gauravyadavpro63@gmail.com>
2026-06-29 17:07:14 +0530

    header-done

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-//header
-// body
-// footer
+const arr=[{cloth:"tshirt",offer:"20-30%off"},{cloth:"shirt",offer:"20-40%off"},{cloth:"kurta",offer:"20-90%off"},{cloth:"jeans",offer:"4-6%off"},{cloth:"T-Shirt",offer:"20% OFF"},{cloth:"Shirt",offer:"35% OFF"},{cloth:"Jeans",offer:"50% OFF"},{cloth:"Hoodie",offer:"15% OFF"},{cloth:"Kurta",offer:"40% OFF"},{cloth:"Jacket",offer:"60% OFF"}];
+
 
 
 function Card(props){
     return(
-<div style={{border:"2px solid black",padding:"2px"}}>
+<div className="card" style={{border:"2px solid black",padding:"2px"}}>
     <img src="https://imgs.search.brave.com/AfSuwap_n2n0j9W5uxav7y-w3phFX2NnZKaC9EuUZOc/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NTFNaFJSeDJYSUwu/X0FDLl9TUjE4MCwy/MzAuanBn" alt="" height="200px" width="200px" />
     <div style={{textAlign:"center"}}>
         <h2>{props.cloth}</h2>
@@ -19,19 +18,47 @@ function Card(props){
     )
 }
 
-const arr=[{cloth:"tshirt",offer:"20-30%off"},{cloth:"shirt",offer:"20-40%off"},{cloth:"kurta",offer:"20-90%off"},{cloth:"jeans",offer:"4-6%off"},{cloth:"T-Shirt",offer:"20% OFF"},{cloth:"Shirt",offer:"35% OFF"},{cloth:"Jeans",offer:"50% OFF"},{cloth:"Hoodie",offer:"15% OFF"},{cloth:"Kurta",offer:"40% OFF"},{cloth:"Jacket",offer:"60% OFF"}];
+function Header(props){
+    return(
+        <div className="heading">
+          <img  className="images" src="https://imgs.search.brave.com/WNSODtjL_DbEDsU9Y6GAhvoTVcnUCAQ2PcdNX5ct_FY/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2M4Lzkz/LzEyL2M4OTMxMjE5/MTVmNDE5NmY2YjBj/NDk5NmZjMDEyYThk/LmpwZw" alt="" height="80px" width="80px"/>
+          <div className="options">
+            <button className="but">Men</button>
+            <button className="but">Women</button>
+            <button className="but">Children</button>
+            <button className="but">Kids</button>
+            <button className="but">Home</button>
+            <button className="but">Beauty</button>
+          </div>
+           <input className="search" type="text" placeholder="search here baby girl" />
+           <div className="profile">
+            <button className="pro">Profile</button>
+            <button className="pro">Wislist</button>
+            <button className="pro">Bag</button>
+           </div>
+        </div>
+    )
+}
+
+
+
+
 
 function App(){
 return (
-    // header
+    <>
+   <Header/>
+  
 
- <div style={{display:"flex",gap:"10px",flexWrap:"wrap"}}>
+ <div className="middle" style={{display:"flex",gap:"10px",flexWrap:"wrap"}}>
   {
     arr.map((value,index)=> <Card key={index} cloth={value.cloth} offer={value.offer}/>)
   
   }
     </div>
-    // footer
+
+  <Footer/>
+    </>
 )
 }
 
