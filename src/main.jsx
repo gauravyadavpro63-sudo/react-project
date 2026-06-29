ec9be9c019ce1fd4bf51da9344e062e6872234a1
Gaurav Yadav <gauravyadavpro63@gmail.com>
2026-06-29 17:19:40 +0530

    footer

--- src/main.jsx
+++ src/main.jsx
@@ -41,7 +41,11 @@ function Header(props){
 }
 
 
-
+function Footer(props){
+    return (
+        <img src="https://assets.myntassets.com/w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2025/NOVEMBER/8/dndfsB1V_65ecb385cb2e4af2b0313364aeae6da8.jpg" width="1500px"></img>
+    )
+}
 
 
 function App(){
